50f4dba5409c4d5a02c4e9df6d97621e8f094d83
Anna Zoi <zoianna01@gmail.com>
2026-02-13 23:28:09 +0200

    fixes

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,6 +1,130 @@
 'use client';
 
 const Skills = () => {
+	const skills = [
+		{
+			name: 'ReactJS',
+			photo: '',
+		},
+		{
+			name: 'Javascript',
+			photo: '',
+		},
+		{
+			name: 'NextJS',
+			photo: '',
+		},
+		{
+			name: 'HTML',
+			photo: '',
+		},
+		{
+			name: 'CSS',
+			photo: '',
+		},
+		{
+			name: 'Firabase',
+			photo: '',
+		},
+		{
+			name: 'MongoDB',
+			photo: '',
+		},
+		{
+			name: 'MariaDB',
+			photo: '',
+		},
+		{
+			name: 'PostgreSQL',
+			photo: '',
+		},
+		{
+			name: 'Typescript',
+			photo: '',
+		},
+		{
+			name: 'WebSockets',
+			photo: '',
+		},
+		{
+			name: 'WebRTC',
+			photo: '',
+		},
+		{
+			name: 'Prisma',
+			photo: '',
+		},
+		{
+			name: 'NodeJS',
+			photo: '',
+		},
+		{
+			name: 'ExpressJS',
+			photo: '',
+		},
+
+		{
+			name: 'NestJS',
+			photo: '',
+		},
+
+		{
+			name: 'PHP',
+			photo: '',
+		},
+
+		{
+			name: 'Java',
+			photo: '',
+		},
+
+		{
+			name: 'Python',
+			photo: '',
+		},
+
+		{
+			name: 'C',
+			photo: '',
+		},
+		{
+			name: 'Prolog',
+			photo: '',
+		},
+		{
+			name: 'Android Studio',
+			photo: '',
+		},
+		{
+			name: 'Ionic',
+			photo: '',
+		},
+		{
+			name: 'Material UI',
+			photo: '',
+		},
+		{
+			name: 'Chakra UI',
+			photo: '',
+		},
+		{
+			name: 'Git',
+			photo: '',
+		},
+		{
+			name: 'Slack',
+			photo: '',
+		},
+		{
+			name: 'Asana',
+			photo: '',
+		},
+		{
+			name: 'Trello',
+			photo: '',
+		},
+	];
+
 	return (
 		<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mt-20 about flex flex-col gap-10 projects">
 			<h3 className="text-center">My Skills</h3>
