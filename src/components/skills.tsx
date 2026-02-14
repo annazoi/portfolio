5df6fc10d4b2e3d5b124757666831bb1a6314dd2
Anna Zoi <zoianna01@gmail.com>
2026-02-14 18:28:24 +0200

    skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,6 +1,31 @@
 'use client';
 import react from '../assets/skills/react.png';
 import javascript from '../assets/skills/javascript.png';
+import nextjs from '../assets/skills/nextjs.png';
+import html from '../assets/skills/html.png';
+import css from '../assets/skills/css.png';
+import firabase from '../assets/skills/firabase.png';
+import mongodb from '../assets/skills/mongodb.png';
+import mariadb from '../assets/skills/mariadb.png';
+import prostgresql from '../assets/skills/postgresql.png';
+import typescript from '../assets/skills/typescript.png';
+import prisma from '../assets/skills/prisma.png';
+import nodejs from '../assets/skills/nodejs.png';
+import expressjs from '../assets/skills/expressjs.png';
+import nestjs from '../assets/skills/nestjs.png';
+import php from '../assets/skills/php.png';
+import java from '../assets/skills/java.png';
+import python from '../assets/skills/python.png';
+import c from '../assets/skills/c.png';
+import prolog from '../assets/skills/prolog.png';
+import androidStudio from '../assets/skills/androidStudio.png';
+import ionic from '../assets/skills/ionic.png';
+import materialUI from '../assets/skills/materialUI.png';
+import chakraUI from '../assets/skills/chakraUI.png';
+import git from '../assets/skills/github.png';
+import slack from '../assets/skills/slack.png';
+import asana from '../assets/skills/asana.png';
+import trello from '../assets/skills/trello.png';
 
 import Image from 'next/image';
 
@@ -16,127 +41,130 @@ const Skills = () => {
 		},
 		{
 			name: 'NextJS',
-			photo: '../assets/skills/nextjs.png',
+			photo: nextjs,
 		},
 		{
 			name: 'HTML',
-			photo: '../assets/skills/html.png',
+			photo: html,
 		},
 		{
 			name: 'CSS',
-			photo: '../assets/skills/css.png',
+			photo: css,
 		},
 		{
 			name: 'Firabase',
-			photo: '../assets/skills/firabase.png',
+			photo: firabase,
 		},
 		{
 			name: 'MongoDB',
-			photo: '../assets/skills/mongodb.png',
+			photo: mongodb,
 		},
 		{
 			name: 'MariaDB',
-			photo: '../assets/skills/mariadb.png',
+			photo: mariadb,
 		},
 		{
 			name: 'PostgreSQL',
-			photo: '../assets/skills/postgresql.png',
+			photo: prostgresql,
 		},
 		{
 			name: 'Typescript',
-			photo: '../assets/skills/typescript.png',
+			photo: typescript,
 		},
 		{
 			name: 'WebSockets',
-			photo: '../assets/skills/asana.png',
+			photo: react,
 		},
 		{
 			name: 'WebRTC',
-			photo: '../assets/skills/asana.png',
+			photo: react,
 		},
 		{
 			name: 'Prisma',
-			photo: '../assets/skills/prisma.png',
+			photo: prisma,
 		},
 		{
 			name: 'NodeJS',
-			photo: '../assets/skills/nodejs.png',
+			photo: nodejs,
 		},
 		{
 			name: 'ExpressJS',
-			photo: '../assets/skills/expressjs.png',
+			photo: expressjs,
 		},
 
 		{
 			name: 'NestJS',
-			photo: '../assets/skills/nestjs.png',
+			photo: nestjs,
 		},
 
 		{
 			name: 'PHP',
-			photo: '../assets/skills/php.png',
+			photo: php,
 		},
 
 		{
 			name: 'Java',
-			photo: '../assets/skills/java.png',
+			photo: java,
 		},
 
 		{
 			name: 'Python',
-			photo: '../assets/skills/python.png',
+			photo: python,
 		},
 
 		{
 			name: 'C',
-			photo: '../assets/skills/c.png',
+			photo: c,
 		},
 		{
 			name: 'Prolog',
-			photo: '../assets/skills/prolog.png',
+			photo: prolog,
 		},
 		{
 			name: 'Android Studio',
-			photo: '../assets/skills/androidStudio.png',
+			photo: androidStudio,
 		},
 		{
 			name: 'Ionic',
-			photo: '../assets/skills/ionic.png',
+			photo: ionic,
 		},
 		{
 			name: 'Material UI',
-			photo: '../assets/skills/materialUI.png',
+			photo: materialUI,
 		},
 		{
 			name: 'Chakra UI',
-			photo: '../assets/skills/chakraUI.png',
+			photo: chakraUI,
 		},
 		{
 			name: 'Git',
-			photo: '../assets/skills/github.png',
+			photo: git,
 		},
 		{
 			name: 'Slack',
-			photo: '../assets/skills/slack.png',
+			photo: slack,
 		},
 		{
 			name: 'Asana',
-			photo: '../assets/skills/asana.png',
+			photo: asana,
 		},
 		{
 			name: 'Trello',
-			photo: '../assets/skills/trello.png',
+			photo: trello,
 		},
 	];
 
 	return (
-		<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mt-20 about flex flex-col gap-10 projects">
+		<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mt-20 about flex flex-col xl:gap-10 projects gap-5">
 			<h3 className="text-center">My Skills</h3>
-			{skills.map((skill, index: number) => (
-				<div key={index}>
-					<Image src={skill.photo} alt={skill.name} width={128} height={128}></Image>
-				</div>
-			))}
+			<div className="grid grid-cols-2  gap-4 xl:grid-cols-6 xl:gap-10 md:grid-cols-4 sm:grid-cols-3 pl-2 ">
+				{skills.map((skill, index: number) => (
+					<div key={index} className="skill flex flex-col xl:gap-6 gap-2">
+						<Image src={skill.photo} alt={skill.name} width={60} height={60} className="m-auto"></Image>
+						<h2 className="text-center">{skill.name}</h2>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
